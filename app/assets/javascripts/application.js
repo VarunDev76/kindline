7ec908f0825c72ca2cd6add90888bf71d40a92ef
manik <manik.impinge@gmail.com>
2017-02-13 18:41:49 +0530

    assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,20 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
+//= require jquery.asScrollable.all
+//= require jquery-asHoverScroll
+//= require jquery.dataTables
+//= require dataTables.bootstrap
+//= require dataTables.responsive
+//= require core
+//= require site
+//= require menu
+//= require menubar
+//= require sidebar
+//= require gridmenu
+//= require formValidation
+//= require formValidation_bootstrap
+//= require datatables
+//= require datatable
 //= require turbolinks
